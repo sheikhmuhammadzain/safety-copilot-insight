5de564427d27d94387934c128f89870565e690ef
sheikhmuhammadzain <zaingee320@gmail.com>
2025-10-07 15:53:10 +0500

    loader add kia ha

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root")!;
+createRoot(rootEl).render(<App />);
+
+// Remove the HTML preloader once React mounts
+window.requestAnimationFrame(() => {
+  const pre = document.getElementById("app-preloader");
+  if (pre) {
+    pre.classList.add("fade-out");
+    // Fully remove after transition
+    setTimeout(() => pre.remove(), 350);
+  }
+});
